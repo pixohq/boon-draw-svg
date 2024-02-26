48f8804200e310a270a6969c7437f690367f97d4
LeeSeongJin <leeseongjinca@pixo.co>
2024-02-26 15:00:57 +0900

    Remove: jest-transformer-svg 모듈 제거
    - jest + react 환경을 고려한 모듈이여서 사용하지 못하기에 제거함

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,8 @@
 
 module.exports = {
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/fileTransformer.js",
+    "^.+\\.ts?$": "ts-jest",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileTransformer.js",
     "\\.(svg)$": "<rootDir>/svgTransformer.js",
   },
   testRegex: ".*\.test\.ts$",
