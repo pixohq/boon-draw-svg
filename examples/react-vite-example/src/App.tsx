e51942c1e1808cc2f7c0feb97bd7461f55105bd6
LeeSeongJin <leeseongjinca@pixo.co>
2024-02-26 13:53:09 +0900

    Feat: react-vite 예제 코드 수정
    - 브랜드명을 입력받아 수정할 수 있도록 함

--- examples/react-vite-example/src/App.tsx
+++ examples/react-vite-example/src/App.tsx
@@ -4,32 +4,47 @@ import { BoonDrawSVG } from 'boon-draw-svg';
 import SVG1 from './assets/6DecQXLpmG9WZJm9f3rVjg.svg'
 import SVG2 from './assets/6DecQXLpmG9WZJm9f3rVjg-brandName.svg';
 
+interface ItemProps {
+  boonDrawer: BoonDrawSVG;
+  id: string;
+  targetId: string;
+  templateSvg: string;
+  brandName?: string;
+}
+
 const fetchSvg = async (url: string, brandName?: string) => {
   // 쿼리스트링 매개변수 객체
   const queryParams = brandName ? { brandName } : undefined;
 
   // URL에 쿼리스트링 추가
   const queryString = new URLSearchParams(queryParams).toString();
-  console.log('queryString',{queryString});
 
   const res = await fetch(`${url}?${queryString}`);
 
   return await res.text();
 }
 
-const svgs = [
+const getQueryString = (key: string) => {
+  const params = new URLSearchParams(window.location.search);
+
+  return params.get(key) ?? undefined;
+};
+
+const svgs: { key: string; targetId: string; templateSvg: string; }[] = [
   // 로컬 파일 테스트
   { key: 'svg1', targetId: '', templateSvg: SVG1 },
   { key: 'svg2', targetId: 'target-dataId', templateSvg: SVG2 },
 
   // 서버 연동 테스트
-  { key: 'svg3', targetId: '', templateSvg: 'http://localhost:3000/svg/6DecQXLpmG9WZJm9f3rVjg' },
-  { key: 'svg4', targetId: 'target-dataId', templateSvg: 'http://localhost:3000/svg/6DecQXLpmG9WZJm9f3rVjg-brandName' },
+  // { key: 'svg3', targetId: '', templateSvg: 'http://localhost:3000/svg/6DecQXLpmG9WZJm9f3rVjg' },
+  // { key: 'svg4', targetId: 'target-dataId', templateSvg: 'http://localhost:3000/svg/6DecQXLpmG9WZJm9f3rVjg-brandName' },
 ];
 
 function App() {
   const boonDrawerRef = useRef(new BoonDrawSVG());
   const boonDrawer = boonDrawerRef.current;
+
+  const [brandName, setBrandName] = useState<string>(getQueryString('brandName') ?? '');
 
   return (
     <>
@@ -40,6 +55,17 @@ function App() {
           alignItems: 'center'
         }}
       >
+        <input
+          type="text"
+          value={brandName} 
+          onChange={(e) => setBrandName(e.currentTarget.value)} 
+          style={{
+            width: 500,
+            height: 50,
+            padding: 16
+          }}
+          />
+        <hr />
         {svgs.map(({ key, targetId, templateSvg }, index) => {
           return (
             <div 
@@ -50,7 +76,13 @@ function App() {
                 padding: 24
               }}
             >
-              <Item boonDrawer={boonDrawer} id={key} targetId={targetId} templateSvg={templateSvg} />
+              <Item
+                boonDrawer={boonDrawer} 
+                id={key} 
+                targetId={targetId} 
+                templateSvg={templateSvg}
+                brandName={brandName}
+              />
             </div>
           )
         })}
@@ -59,25 +91,41 @@ function App() {
   )
 }
 
-const Item = ({ boonDrawer, id: key, targetId, templateSvg }: { boonDrawer: BoonDrawSVG; id: string; targetId: string; templateSvg: string; }) => {
+const Item = ({ boonDrawer, id: key, targetId, templateSvg, brandName }: ItemProps) => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [svgString, setSvgString] = useState<string>(templateSvg);
 
   useEffect(() => {
-    fetchSvg(templateSvg, 'Hello')
+    fetchSvg(templateSvg, brandName)
       .then(async (svgString) => {
-        console.log(key);
-        svgString = (
+        svgString = (brandName ? (
             await boonDrawer
-            // .initUniqueId(id, svgString, targetId)
-            .init(key, svgString, targetId)
-            // .updateBrandName({ key, brandName: 'Hello!', targetId })
-          )
+            // .init(key, svgString, targetId)
+            .initUniqueId(key, svgString, targetId)
+            .updateBrandName({ key, brandName, targetId })
+          ) : (boonDrawer
+            .initUniqueId(key, svgString, targetId)))
           .setFullWidth(key)
           .getSvgString(key);
 
         setSvgString(svgString);
+      }).finally(() => {
+        setIsLoaded(true);
       })
   }, [boonDrawer, key, targetId, templateSvg]);
+
+  useEffect(() => {
+    if (isLoaded === false) return;
+    if (brandName) {
+      (async () => {
+        const svgString = (await boonDrawer
+          .updateBrandName({ key, targetId, brandName }))
+          .getSvgString(key);
+
+        setSvgString(svgString);
+      })()
+    }
+  }, [brandName])
 
   return <div dangerouslySetInnerHTML={{ __html: svgString }} />
 }
