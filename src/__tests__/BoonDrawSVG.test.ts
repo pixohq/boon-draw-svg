61750451922225a07fb83dc1f9f661a233cb56f3
LeeSeongJin <leeseongjinca@pixo.co>
2024-02-26 14:59:06 +0900

    Fix: 테스트 코드에 사용하는 상수 변수 선언부 수정
    - 코드 최상단으로 끌어올림

--- src/__tests__/BoonDrawSVG.test.ts
+++ src/__tests__/BoonDrawSVG.test.ts
@@ -5,6 +5,9 @@ import SVG from './assets/6DecQXLpmG9WZJm9f3rVjg.svg';
 import SVG_BRAND_NAME from './assets/6DecQXLpmG9WZJm9f3rVjg-brandName.svg';
 import { SVG_STRING } from './constants';
 
+const TEST_KEY = 'template-key';
+const TEST_TARGET_ID = 'target-dataId';
+
 describe('BoonDrawSVG 모듈', () => {
   test('생성자 테스트', () => {
     const boonDrawSvg = new BoonDrawSVG();
@@ -13,68 +16,58 @@ describe('BoonDrawSVG 모듈', () => {
   });
 
   describe('init() 테스트', () => {
-    const key = 'template-key';
-    const targetId = 'target-dataId';
     const boonDrawSvg = new BoonDrawSVG();
 
     test('초기화 테스트', () => {
-      expect(boonDrawSvg.init(key, SVG_STRING, targetId).getSvgString(key)).toMatch('svg');
+      expect(boonDrawSvg.init(TEST_KEY, SVG_STRING, TEST_TARGET_ID).getSvgString(TEST_KEY)).toMatch('svg');
     });
 
     test('아이디가 교체되지 않아야 함', () => {
-      expect(boonDrawSvg.init(key, SVG_STRING, targetId).getSvgString(key)).toMatch('id="canvas1-clip"');
+      expect(boonDrawSvg.init(TEST_KEY, SVG_STRING, TEST_TARGET_ID).getSvgString(TEST_KEY)).toMatch('id="canvas1-clip"');
     });
   });
 
   describe('initWithUniqueId() 테스트', () => {
-    const key = 'template-key';
-    const targetId = 'target-dataId';
     const boonDrawSvg = new BoonDrawSVG();
 
     test('초기화 테스트', () => {
-      expect(boonDrawSvg.initUniqueId(key, SVG_STRING, targetId).getSvgString(key)).toMatch('svg');
+      expect(boonDrawSvg.initUniqueId(TEST_KEY, SVG_STRING, TEST_TARGET_ID).getSvgString(TEST_KEY)).toMatch('svg');
     });
 
     test('아이디가 초기화 되어야 함', () => {
-      expect(boonDrawSvg.initUniqueId(key, SVG_STRING, targetId).getSvgString(key)).not.toMatch('id="canvas1-clip"');
+      expect(boonDrawSvg.initUniqueId(TEST_KEY, SVG_STRING, TEST_TARGET_ID).getSvgString(TEST_KEY)).not.toMatch('id="canvas1-clip"');
     });
   });
 
   describe('<text> 테스트', () => {
     test('"A happy accident happened" 텍스트가 존재해야 함', () => {
-      const key = 'template-key';
-      const targetId = 'target-dataId';
       const boonDrawSvg = new BoonDrawSVG();
-      const svgString = boonDrawSvg.initUniqueId(key, SVG_STRING, targetId).getSvgString(key);
+      const svgString = boonDrawSvg.initUniqueId(TEST_KEY, SVG_STRING, TEST_TARGET_ID).getSvgString(TEST_KEY);
 
       expect(svgString).toMatch('A happy accident happened');
     });
 
     test('brandNameId가 존재하지 않는 경우 에러가 발생해야 함', async () => {
-      const key = 'template-key';
-      const targetId = 'target-dataId';
       const boonDrawSvg = new BoonDrawSVG();
 
       try {
         await boonDrawSvg
-          .initUniqueId(key, SVG, targetId)
-          .updateBrandName({ key, targetId, brandName: 'A New Text' });
+          .initUniqueId(TEST_KEY, SVG, TEST_TARGET_ID)
+          .updateBrandName({ key: TEST_KEY, targetId: TEST_TARGET_ID, brandName: 'A New Text' });
       } catch (error) {
         expect(error).toEqual(new Error('텍스트 엘리먼트를 찾을 수 없습니다.'));
       }
     });
 
     test('brandNameId가 존재하는 경우 텍스트가 변경되어야 함', async () => {
-      const key = 'template-key';
-      const targetId = 'target-dataId';
       const boonDrawSvg = new BoonDrawSVG();
 
       try {
         const svgString = (
           await boonDrawSvg
-            .initUniqueId(key, SVG_BRAND_NAME, targetId)
-            .updateBrandName({ key, targetId, brandName: 'A New Text' })
-        ).getSvgString(key);
+            .initUniqueId(TEST_KEY, SVG_BRAND_NAME, TEST_TARGET_ID)
+            .updateBrandName({ key: TEST_KEY, targetId: TEST_TARGET_ID, brandName: 'A New Text' })
+        ).getSvgString(TEST_KEY);
 
         expect(svgString).toMatch('A New Text');
       } catch (error) {
